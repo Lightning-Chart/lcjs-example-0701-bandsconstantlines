03533b945d22d94b50e603d32c141453061e2c93
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:11:24 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ const { createProgressiveTraceGenerator } = xydata
 const chartTitle = 'Bands and Constantlines'
 
 // Create a XY Chart.
-const chart = lightningChart()
+const chart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
