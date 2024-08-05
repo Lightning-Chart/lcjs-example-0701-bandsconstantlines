cf78d93890ad2067c4a0aa2b93dd8ba50c219798
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 09:53:07 +0300

    v6.0.0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * LightningChartJS example that showcases basic usage of Bands and Constantlines.
  */
 // Import LightningChartJS
-const lcjs = require('@arction/lcjs')
+const lcjs = require('@lightningchart/lcjs')
 
 // Import xydata
-const xydata = require('@arction/xydata')
+const xydata = require('@lightningchart/xydata')
 
 // Extract required parts from LightningChartJS.
 const { lightningChart, Themes } = lcjs
